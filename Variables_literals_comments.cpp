1314f2408aea015de72cd3ebe81700706a0fb2a7
Akshata Gunapache <agunapache28@gmail.com>
2023-07-10 08:22:50 +0530

    Create Variables_literals_comments.cpp

---
+++ Variables_literals_comments.cpp
@@ -0,0 +1,40 @@
+//Variables,literals and comments
+/*Different ways of intialzing a variable in c++
+1: int a = 5;
+2: int a(5);
+3: int a{5};
+4: auto a = 5;  using auto class: it tells the type of the variable upon its initialization
+5: int a;  dynamic initialization
+   cin>>a;
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int sum = 6;  //Variables are containeres to store/hold data
+    cout<<"Hello World"<< sum;
+    return 0;
+}
+
+/*this
+is 
+a 
+sample comment
+.
+*/
+
+
+//Note: int is used to store only integers
+// double is used to store decimals and exponentials
+
+
+/*
+What are literals?
+Literals are data used to store fixed values. There are differenct types of literals:
+1) Integers : decimal , octal , hexadecimal
+2) Floating-point literals: fractional/exponential form
+3) Characters
+4) Escape sequences : \n , \0 , \t ,etc
+5) String literals : "" , ''
+*/
